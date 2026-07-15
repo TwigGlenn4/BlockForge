0dbf91c235eafd79291a209f70f8a54fc6bb870a
TwigGlenn4 <twigglenn4@gmail.com>
2026-07-15 09:11:44 -0500

    make hotbar slots selectable

--- scenes/GameScene/hotbar_slot.gd
+++ scenes/GameScene/hotbar_slot.gd
@@ -1,11 +1,28 @@
 class_name HotbarSlot
 extends Control
 
+signal slot_selected(slot_number)
+
 @export var button_bg: TextureButton
 @export var item_icon: TextureRect
 @export var item_count_label: Label
 
+@export var bg_texture_unselected: Texture2D
+@export var bg_texture_selected: Texture2D
+
+
 var slot_num: int
+var selected: bool:
+	set(value):
+		if selected != value: # if selected has changed
+			if value == true:
+				button_bg.texture_normal = bg_texture_selected
+			else:
+				button_bg.texture_normal = bg_texture_unselected
+		selected = value
+
+		
+
 
 func setup(num: int) -> void:
 	slot_num = num
@@ -33,4 +50,9 @@ func set_stack(stack: ItemStack) -> void:
 
 func _on_button_bg_pressed() -> void:
 	print("[HotbarSlot] slot ", slot_num, " clicked.")
+	slot_selected.emit(slot_num)
+
+
+
+
 
--- scenes/GameScene/inventory_ui.gd
+++ scenes/GameScene/inventory_ui.gd
@@ -1,15 +1,17 @@
 extends Control
 
-const HOTBAR_SLOTS: int = 8
+const HOTBAR_NUM_SLOTS: int = 8
 static var SLOT_TEXTURE = load("res://assets/textures/atlas/inventory_slot.png")
 
 static var HOTBAR_SLOT_SCENE = preload("res://scenes/GameScene/HotbarSlot/HotbarSlot.tscn")
 
 var hotbar: Array[HotbarSlot]
 var selected_character: Character
+var selected_slot: int = 0
 
 func _ready() -> void:
-	_build_hotbar(HOTBAR_SLOTS)
+	_build_hotbar(HOTBAR_NUM_SLOTS)
+	_on_hotbar_slot_selected(0)
 
 	get_tree().get_root().size_changed.connect(_window_resized)
 	_window_resized()
@@ -41,20 +43,36 @@ func _update_inventory_contents() -> void:
 		hotbar[i].set_stack(inv.contents[i])
 
 func _build_hotbar( num_slots: int) -> void:
-	var hotbar_slot_size: float = 1.0 / HOTBAR_SLOTS ## Hotbar slot size in anchor units
+	var hotbar_slot_size: float = 1.0 / HOTBAR_NUM_SLOTS ## Hotbar slot size in anchor units
 
 	hotbar.resize(num_slots)
 	for i: int in num_slots:
 		hotbar[i] = _build_hotbar_slot(i)
 		add_child(hotbar[i])
+
+		# position the slot
 		var anchor_from_start: float = hotbar_slot_size * i  ## Anchor value for start of hotbar, this var exists to simplify vertical/horizontal hotbar implementation.
 		var anchor_from_end: float = anchor_from_start + hotbar_slot_size  ## Anchor value for end of hotbar, this var exists to simplify vertical/horizontal hotbar implementation.
-
 		hotbar[i].anchor_top = anchor_from_start
 		hotbar[i].anchor_bottom = anchor_from_end
 		# print("[Hotbar] set slot " + str(i) + " anchored from " + str(anchor_from_start) + " to " + str(anchor_from_end))
+
+		# attach hotbar_slot.slot_selected signal
+		hotbar[i].slot_selected.connect(_on_hotbar_slot_selected)
 
 func _build_hotbar_slot(num: int) -> Control:
 	var slot: Control = HOTBAR_SLOT_SCENE.instantiate()
 	slot.setup(num)
 	return slot	
+
+func _on_hotbar_slot_selected(selected_slot_num: int) -> void:
+	selected_slot = selected_slot_num
+	for slot_num in HOTBAR_NUM_SLOTS:
+		if slot_num == selected_slot_num: # mark the selected slot as selected
+			hotbar[slot_num].selected = true
+		else:                             # mark all other slots as unselected
+			hotbar[slot_num].selected = false
+
+
+func get_held_item_stack() -> ItemStack:
+	return selected_character.inventory.contents[selected_slot]
